328e66804eddcb1037b7063ea77d1a98911582de
xieyx <285837460@qq.com>
2017-08-04 12:35:09 +0800

    syslink

---
+++ syslink/src/main/java/com/tongyuan/SyslinkApplication.java
@@ -0,0 +1,50 @@
+package com.tongyuan;
+
+import com.tongyuan.client.CxfClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+@SpringBootApplication
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+public class SyslinkApplication extends SpringBootServletInitializer {
+    @Autowired
+	private static CxfClient cxfClient;
+/*    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }*/
+    /**
+     * 实现SpringBootServletInitializer可以让spring-boot项目在web容器中运行
+     */
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        builder.sources(this.getClass());
+        return super.configure(builder);
+    }
+
+    public static void main(String[] args) {
+		SpringApplication.run(SyslinkApplication.class, args);
+		cxfClient.cl1();
+	}
+
+    //增加一个SpringMVC的DispatcherServlet，接收前台/api开头的请求
+    @Bean
+    public ServletRegistrationBean apiV1ServletBean(WebApplicationContext wac) {
+        DispatcherServlet servlet = new DispatcherServlet(wac);
+        ServletRegistrationBean bean = new ServletRegistrationBean(servlet, "/api");
+        bean.setName("ApiServlet");
+        return bean;
+    }
+}
