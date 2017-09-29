cf3493bb90b13592872863db675ac222e9636d03
YangHuaGit <825095884@qq.com>
2017-09-29 08:56:41 +0800

    启动类

--- syslink/src/main/java/com/tongyuan/SyslinkApplication.java
+++ syslink/src/main/java/com/tongyuan/SyslinkApplication.java
@@ -5,22 +5,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 
-@SpringBootApplication(exclude = {
-        DataSourceAutoConfiguration.class
-})
+import javax.servlet.MultipartConfigElement;
+
+@SpringBootApplication
 @Configuration
 @ComponentScan
 @EnableAutoConfiguration
 public class SyslinkApplication extends SpringBootServletInitializer {
     @Autowired
-	private static CxfClient cxfClient;
+    private static CxfClient cxfClient;
 /*    @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(Application.class);
@@ -35,9 +35,23 @@ public class SyslinkApplication extends SpringBootServletInitializer {
     }
 
     public static void main(String[] args) {
-		SpringApplication.run(SyslinkApplication.class, args);
-		cxfClient.cl1();
-	}
+        SpringApplication.run(SyslinkApplication.class, args);
+        cxfClient.cl1();
+    }
+
+    /**
+     * 文件上传配置
+     * @return
+     */
+    @Bean
+    public MultipartConfigElement multipartConfigElement() {
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        //文件最大
+        factory.setMaxFileSize("102400KB"); //KB,MB
+        /// 设置总上传数据总大小
+        factory.setMaxRequestSize("1024000KB");
+        return factory.createMultipartConfig();
+    }
 
     //增加一个SpringMVC的DispatcherServlet，接收前台/api开头的请求
 /*    @Bean
