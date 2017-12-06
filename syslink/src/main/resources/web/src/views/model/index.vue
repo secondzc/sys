956c395d5e0587ecc6d63e3fc57130f2f82337db
admin <gogs@fake.local>
2017-12-06 11:31:55 +0800

    debug

--- syslink/src/main/resources/web/src/views/model/index.vue
+++ syslink/src/main/resources/web/src/views/model/index.vue
@@ -769,9 +769,6 @@
             });
         },
         handleDeleted(index, row){
-           this.$confirm('确认删除该用户吗?', '提示', {
-          type: 'warning'
-        }).then(() => {
             console.log(index, row);
             var _this = this;
             var url = '/api/model/deleted?modelId=' + row.parentId;
@@ -795,9 +792,7 @@
                 }).catch(function (error) {
                 console.log(error);
             });
-                 }).catch(() => {
 
-        });
         },
         addStar(item){
             if (item.alreadyStar == false) {
